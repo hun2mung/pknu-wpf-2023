3ee01e3c41f413338bad822c8516e2e8f2fcc844
hun2mung <ldh5444@naver.com>
2023-04-19 17:27:53 +0900

    23-04-19

---
+++ Day02/Day02WpfApp/wp05_bikeshop/Logics/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace wp05_bikeshop.Logics
+{
+    public class Car
+    {
+        public string Names {  get; set; }   // Auto Property
+        public double Speed { get; set; }
+        public Color Colors { get; set; }
+
+        //public Human Driver { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -11,4 +11,11 @@ WPF : Windows Presentation Foundation
 ## 2일차
 - WPF 기초
     - 기본 컨트롤
-    - 레이아웃 Grid...
+    - 레이아웃 Grid 사용방법
+    - Bikeshop 시작
+    - WPF만의 개발방식
+    - 데이터 바인딩
+
+## 3일차
+- WPF 기초
+    - 데이터바인딩 계속
