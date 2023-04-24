61b934725238928eba1df0d40f2e3f862cb4491f
hun2mung <ldh5444@naver.com>
2023-04-24 12:25:53 +0900

    23-04-24

---
+++ Day05/Day05WpfApp/wp09_caliburnApp/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using wp09_caliburnApp.ViewModels;
+
+namespace wp09_caliburnApp
+{
+    // Caliburn으로 MVVM 실행할 때 주요 설정 진행
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();   // Caliburn MVVM 초기화
+        }
+
+        // 시작한 후에 초기화 진행
+        protected async override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // base.Onstartup(sender, e); // 부모클래스 실행은 주석
+            await DisplayRootViewForAsync<Main>();
+        }
+    }
+}
